e405ee9c0c5692f238d40c89408afb64735d0dcd
kalebunna <siconfix@gmail.com>
2025-07-07 07:18:07 +0700

    add loho

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <h1 class="auth-title">Log in.</h1>
+    <h1 class="auth-title">Log in</h1>
     <p class="auth-subtitle mb-5">Masuk dengan data yang Anda masukkan saat pendaftaran.</p>
 
     <!-- Session Status -->
@@ -9,7 +9,8 @@
         @csrf
 
         <div class="form-group position-relative has-icon-left mb-4">
-            <input type="email" name="email" id="email" class="form-control form-control-xl" placeholder="Email" value="{{ old('email') }}" required autofocus>
+            <input type="email" name="email" id="email" class="form-control form-control-xl" placeholder="Email"
+                value="{{ old('email') }}" required autofocus>
             <div class="form-control-icon">
                 <i class="bi bi-person"></i>
             </div>
@@ -17,11 +18,13 @@
         </div>
 
         <div class="form-group position-relative has-icon-left mb-4">
-            <input type="password" name="password" id="password" class="form-control form-control-xl" placeholder="Password" required autocomplete="current-password">
+            <input type="password" name="password" id="password" class="form-control form-control-xl"
+                placeholder="Password" required autocomplete="current-password">
             <div class="form-control-icon">
                 <i class="bi bi-shield-lock"></i>
             </div>
-            <div class="form-control-icon-right" style="position: absolute; right: 15px; top: 50%; transform: translateY(-50%); cursor: pointer;">
+            <div class="form-control-icon-right"
+                style="position: absolute; right: 15px; top: 50%; transform: translateY(-50%); cursor: pointer;">
                 <i class="bi bi-eye" id="togglePassword" onclick="togglePasswordVisibility()"></i>
             </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
@@ -46,7 +49,7 @@
         function togglePasswordVisibility() {
             const passwordInput = document.getElementById('password');
             const toggleIcon = document.getElementById('togglePassword');
-            
+
             if (passwordInput.type === 'password') {
                 passwordInput.type = 'text';
                 toggleIcon.classList.remove('bi-eye');
